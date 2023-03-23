c5b3d091f488aab8fe641780c3d10f66130113ab
Apfirebolt <aspper20@gmail.com>
2023-03-23 23:36:41 +0530

    feat: added open api request

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,56 +1,77 @@
 <template>
-    <div>
-        <p>
-            Questions
-        </p>
-    </div>
-    <n-space>
-        <n-button>Default</n-button>
-        <n-button type="primary">
-            Primary
-        </n-button>
-    </n-space>
+    <n-grid x-gap="12" :cols="1">
+        <n-gi>
+            <n-input v-model:value="inputText" type="textarea"
+                placeholder="Please type in your interview questions here!" />
+        </n-gi>
+        <n-gi>
+            <n-button type="primary" @click="callApiData()">
+                Submit Question
+            </n-button>
+        </n-gi>
+        <n-gi>
+            <n-input v-model:value="outputText" type="textarea" placeholder="Your answers would be here!" :autosize="{
+                minRows: 3
+            }" />
 
-    <n-input
-      v-model:value="inputText"
-      type="textarea"
-      placeholder="Please type in your interview questions here!"
-    />
-    {{ inputText }}
-
-
+        </n-gi>
+    </n-grid>
 </template>
   
 <script>
 
 import { onMounted, defineComponent, ref } from 'vue';
-import { NButton, NSpace, NInput } from 'naive-ui';
+import { NButton, NSpace, NInput, NGrid, NGi, NH1 } from 'naive-ui';
 import axios from 'axios';
 
-export default defineComponent ({
+export default defineComponent({
     name: 'Home',
     components: {
         NButton,
         NSpace,
-        NInput
+        NInput,
+        NGrid,
+        NGi,
+        NH1
     },
     setup() {
         const apiData = ref(null)
         const inputText = ref(null)
+        const outputText = ref(null)
 
         const callApiData = async () => {
-            const responseData = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-            apiData.value = responseData.data
-            console.log(responseData)
-            
+
+            const headers = {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+            }
+            const jsonData = {
+                "model": "text-davinci-003",
+                "prompt": inputText.value,
+                "temperature": 0.5,
+                "max_tokens": 150,
+                "top_p": 1.0,
+                "frequency_penalty": 0.0,
+                "presence_penalty": 0.0
+            }
+            try {
+                const responseData = await axios.post(import.meta.env.VITE_OPENAI_API_URL, jsonData, {
+                    headers: headers
+                })
+                console.log(responseData)
+                outputText.value = responseData.data.choices[0].text;
+            } catch (err) {
+                console.log(err)
+            }
+
         }
-        onMounted(() => {
-            callApiData()
-        })
 
         return {
             apiData,
-            inputText
+            inputText,
+            outputText,
+
+            callApiData
         }
     }
 })
@@ -59,5 +80,6 @@ export default defineComponent ({
 <style scoped>
 .n-input {
     margin: 1.5rem auto;
+    width: 500px;
 }
 </style>  
