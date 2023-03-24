d1a712cd157e38468705dbb2e6610826dc82cff1
Apfirebolt <aspper20@gmail.com>
2023-03-24 08:13:16 +0530

    modified sample page, license added

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,16 +1,43 @@
 <template>
-    <p>
-        A sample questions page
-    </p>
-</template>
+    <n-dropdown trigger="hover" :options="options" @select="handleSelect">
+      <n-button type="info">Go For a Trip</n-button>
+    </n-dropdown>
+  </template>
   
-<script>
-
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-    name: 'QuestionsPage',
-})
-
-</script>
-<style scoped></style>  
+  <script>
+  import { defineComponent } from "vue";
+  import { useMessage, NDropdown, NButton } from "naive-ui";
+  
+  export default defineComponent({
+    components: {
+        NDropdown,
+        NButton
+    },
+    setup() {
+      return {
+        options: [
+          {
+            label: "Marina Bay Sands",
+            key: "marina bay sands",
+            disabled: true
+          },
+          {
+            label: "Brown's Hotel, London",
+            key: "brown's hotel, london"
+          },
+          {
+            label: "Atlantis Bahamas, Nassau",
+            key: "atlantis nahamas, nassau"
+          },
+          {
+            label: "The Beverly Hills Hotel, Los Angeles",
+            key: "the beverly hills hotel, los angeles"
+          }
+        ],
+        handleSelect(key) {
+          console.log('Handle select ', key)
+        }
+      };
+    }
+  });
+  </script>
